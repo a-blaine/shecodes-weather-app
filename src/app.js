81817fa633621c17d35ce79485a5b7f0278e4ad9
a-blaine <ai.blaine4@protonmail.com>
2023-03-16 16:47:12 +0000

    Created date and time functions

--- src/app.js
+++ src/app.js
@@ -1,5 +1,50 @@
+function formatDate(timestamp) {
+  let current = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let day = days[current.getDay()];
+  let date = current.getDate();
+  let month = months[current.getMonth()];
+  let year = current.getFullYear();
+  return `${day} ${date} ${month}, ${year}`;
+}
+
+function formatTime(timestamp) {
+  let time = new Date(timestamp);
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
-  console.log(response.data);
   let newTemperature = Math.round(response.data.main.temp);
   let center = document.querySelector("#current-temperature");
   center.innerHTML = `${newTemperature}`;
@@ -35,6 +80,18 @@ function showTemperature(response) {
   let windSpeed = Math.round(response.data.wind.speed);
   let displayWind = document.querySelector("#wind");
   displayWind.innerHTML = ` ${windSpeed}km/h`;
+
+  let displayDate = document.querySelector("#date");
+  displayDate.innerHTML = formatDate(response.data.dt * 1000);
+
+  let displayTime = document.querySelector("#time");
+  displayTime.innerHTML = formatTime(response.data.dt * 1000);
+
+  let degreesC = document.querySelector("#celsius-link");
+  degreesC.classList.add("active");
+
+  let degreesF = document.querySelector("#fahrenheit-link");
+  degreesF.classList.add("inactive");
 }
 
 let city = "Seattle";
